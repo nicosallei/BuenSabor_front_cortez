091465e18ecc55d69b9ad4419786a545e04437ee
sallei <75142980+nicosallei@users.noreply.github.com>
2024-07-29 01:42:16 -0300

    actualizando

--- src/components/auth0/Auth0ProviderWithNavigate.tsx
+++ src/components/auth0/Auth0ProviderWithNavigate.tsx
@@ -11,9 +11,8 @@ export const Auth0ProviderWithNavigate = ({ children }: Props) => {
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
-
+  const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
   //const redirectUri = window.location.origin + "/callback";
-
 
   const onRedirectCallback = (appState: AppState | undefined) => {
     navigate(appState?.returnTo || window.location.pathname);
