c3d77854c8817c18d62db1bf56db1eb4f3cb28a2
sallei <75142980+nicosallei@users.noreply.github.com>
2024-07-28 23:18:50 -0300

    actualizando

--- src/components/auth0/CallbackPage.tsx
+++ src/components/auth0/CallbackPage.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
-const Callback = () => {
+const CallbackPage = () => {
   const { isAuthenticated, handleRedirectCallback } = useAuth0();
   const navigate = useNavigate();
 
@@ -11,19 +11,17 @@ const Callback = () => {
       try {
         await handleRedirectCallback();
         if (isAuthenticated) {
-          navigate("/"); // Redirige a la página principal o a la deseada
+          navigate("/");
         }
       } catch (error) {
         console.error("Error handling callback: ", error);
       }
     };
 
-    if (!isAuthenticated) {
-      processCallback();
-    }
+    processCallback();
   }, [handleRedirectCallback, isAuthenticated, navigate]);
 
   return <div>Loading...</div>;
 };
 
-export default Callback;
+export default CallbackPage;
